e6a249fea89690366d28f5f20f1bb688ad851cd9
Jasper van der Meer <jaspervdmeer@gmail.com>
2022-05-31 18:36:46 +0200

    added README

---
+++ README.md
@@ -0,0 +1,5 @@
+# no-u bot
+a 'bot' that replies with no-u answers whenever you call no-u.php
+
+## Example
+[https://jasper.monster/no-u/no-u.php](https://jasper.monster/no-u/no-u.php).
--- no-u.php
+++ no-u.php
@@ -1,5 +1,5 @@
 <?php
-    $f_contents = file("no-u.txt"); 
+    $f_contents = file("https://raw.githubusercontent.com/jasperfrontend/no-u/master/no-u.txt"); 
     $line = $f_contents[rand(0, count($f_contents) - 1)];
     echo rtrim($line);
 ?>
