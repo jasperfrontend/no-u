6cdb786a1bf17f02df67079d000f96b4c670f57d
Jasper van der Meer <jaspervdmeer@gmail.com>
2022-06-09 22:02:55 +0200

    Added if-statement to check if the txt file exists

--- no-u.php
+++ no-u.php
@@ -1,5 +1,9 @@
 <?php
     $f_contents = file("https://raw.githubusercontent.com/jasperfrontend/no-u/master/no-u.txt"); 
-    $line = $f_contents[rand(0, count($f_contents) - 1)];
-    echo rtrim($line);
+    if($f_contents) {
+        $line = $f_contents[rand(0, count($f_contents) - 1)];
+        echo rtrim($line);
+    } else {
+        echo "ERROR: No data received.";
+    }
 ?>
